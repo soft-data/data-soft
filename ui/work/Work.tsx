b03f149e3680928a52146ed766fbdc9b73dbe651
madrimov <verve@madrimov.madrimov>
2022-12-09 19:41:09 +0500

    work animation

--- ui/work/Work.tsx
+++ ui/work/Work.tsx
@@ -1,7 +1,42 @@
+'use client'
 import React from 'react'
+import { motion, TargetAndTransition } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+const workVariants: {
+	show: TargetAndTransition
+	hidden: TargetAndTransition
+} = {
+	show: {
+		opacity: 1,
+		y: 0
+	},
+	hidden: {
+		opacity: 0,
+		y: -20
+	}
+}
 
 const Work = () => {
-	return <div>Work</div>
+	const { ref, inView, entry } = useInView({
+		rootMargin: '-150px',
+    // initialInView: false,
+    delay: 1000,
+	})
+	return (
+		<motion.div
+			ref={ref}
+			initial='hidden'
+			variants={workVariants}
+			animate={inView ? 'show' : 'hidden'}
+			style={{
+				background: 'red',
+				height: '100vh'
+			}}
+		>
+			Work
+		</motion.div>
+	)
 }
 
 export default Work
